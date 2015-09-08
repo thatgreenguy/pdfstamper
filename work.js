080e16e215edf39b02590ac776afd5cb4f79a180
Paul Green <thatgreenguy@gmx.co.uk>
2015-09-08 14:08:19 +0100

    Replaced remote mounts shell script with node script - need to plumb it ll together now

--- work.js
+++ work.js
@@ -1,230 +1,179 @@
 
-// process record.............
+var async = require( 'async' ),
+  log = require( './common/logger' ),
+  exec = require( 'child_process' ).exec,
+  remoteJdeDir = '/jdedwards/e812/PrintQueue',
+  remoteWorkDir = '/home/pdfprint',
+  localJdeDir = '/home/pdfdata',
+  localWorkDir = '/home/shareddata',
+  sshfsPassword = 'cea63b06a9',
+  sshfsUser = 'pdfprint',
+  sshfsHost = '172.31.240.241',
+  sshfsServerKeepaliveSeconds = 30;
 
-	} else if (rows.length > 0)
-	{
 
-		// Query read has returned a record so we have a valid eligible PDF file to process
+checkRemoteMounts();
 
-		var record = rows[0];
 
-		// Multiple pdfhandler processes could be running so need to establish exclusive rights to 
-		// process this PDF file - if not simply move onto next eligible PDF file to process.
 
-		lock.gainExclusivity(record, hostname, connection, processLockedPdfFile);		
-		
-		// Read next record 
+// Check mounts in place - once established they should remain semi-permanent
+// Establish or re-establish mount directories to JDE Enterprise server on AIX.
+function checkRemoteMounts() {
+  async.series([
+    function ( cb ) { checkJdeQueueMounted( cb ) }, 
+    function ( cb ) { checkWorkDirMounted( cb ) } 	
+    ], function( err, results ) {
+         if ( err ) {
+           log.warn( 'Problem with remote mounts - attempting auto recovery' );
+	   establishRemoteMounts();
+         } else {
+           log.debug( 'Remote mounts okay' );  
+         }
+       }
+    );
+}
 
-        	fetchRowsFromRS(connection, resultSet, numRows, audit);
-	}
+
+// When issue detected with remote mounts to AIX Jde Enterprise server then establish or re-establish them
+function establishRemoteMounts() {
+  async.series([
+    function ( cb ) { unmountJdeQueue( cb ) }, 
+    function ( cb ) { unmountSharedWorkDir( cb ) }, 	
+    function ( cb ) { mountJdeQueue( cb ) },
+    function ( cb ) { mountSharedWorkDir( cb ) } 	
+    ], function( err, results ) {
+         if ( err ) {
+           log.error( 'Unable to establish Remote mounts to AIX (JDE Enterprise Server)' );
+         } else {
+           log.info( 'Remote mounts established' );  
+         }
+       }
+    );
+}
+
+
+// Establish mount to remote JDE enterprise server (AIX) system for JDE Print Queue access/monitoring 
+function mountJdeQueue( cb) {
+  
+  var cmd;
+
+  cmd = 'echo ' + sshfsPassword + ' | ';
+  cmd += 'sshfs -o reconnect -C -o workaround=all -o ServerAliveInterval='
+  cmd += sshfsServerKeepaliveSeconds;
+  cmd += ' -o Ciphers=arcfour  -o cache=no -o password_stdin '
+  cmd += sshfsUser + '@' + sshfsHost + ':' + remoteJdeDir + ' ' +  localJdeDir;  
+  
+  exec( cmd, function( err, stdout, stderr ) {
+    if ( err !== null ) {
+      log.error( cmd + ' - Failed');  
+      cb( err, cmd + ' - Failed' );
+    } else {
+      log.debug( cmd + ' - Done' );  
+      cb(null, cmd + ' - Done');
+    }
   });
 }
 
 
+// Establish mount to remote JDE enterprise server (AIX) system to use as a work area.
+// Process will copy original PDF files here then manipulate them then copy new files back to JDE Output queue
+// Files created here are transient and should be removed by this process immediately or periodically.
+// If the pdfmonitor process is not running files in /home/pdfprint (work directory) can be safely removed 
+function mountSharedWorkDir( cb) {
+  
+  var cmd;
 
+  cmd = 'echo ' + sshfsPassword + ' | ';
+  cmd += 'sshfs -o reconnect -C -o workaround=all -o ServerAliveInterval='
+  cmd += sshfsServerKeepaliveSeconds;
+  cmd += ' -o Ciphers=arcfour  -o cache=no -o password_stdin '
+  cmd += sshfsUser + '@' + sshfsHost + ':' + remoteWorkDir + ' ' +  localWorkDir;  
 
-// Called when exclusive lock has been successfully placed to process the PDF file
-
-function processLockedPdfFile(connection, record) 
-{
-
-	console.log(record[0] + ' >>>>> Lock established');
-
-	// Check this PDF file has definitely not yet been processed by any other pdfHandler instance
-	// that may be running concurrently
-
-	var query = "SELECT COUNT(*) FROM testdta.F559859 WHERE pafndfuf2 = '";
-	query += record[0] + "'";
-
-	connection.execute(query, [], { }, function(err, result) 
-	{
-		if (err) { console.log(err.message); return; };
-
-		var countRec = result.rows[0];
-		var count = countRec[0];
-
-		if ( count > 0 ) {
-			console.log(record[0] + ' >>>>> Already Processed - Releasing Lock.');
-			lock.removeLock(record, hostname);
-
-		} else {
-			console.log(record[0] + ' >>>>> Processing Now.');
-			// This PDF file has not yet been processed and we have the lock so process it now.	
-
-			processPDF(record);
-		}
-
-	}); 
+  exec( cmd, function( err, stdout, stderr ) {
+    if ( err !== null ) {
+      log.error( cmd + ' - Failed');  
+      cb( err, cmd + ' - Failed' );
+    } else {
+      log.debug( cmd + ' - Done' );  
+      cb(null, cmd + ' - Done');
+    }
+  });
 }
 
 
+// Before attempting a mount perform an umount - start from clean state
+function unmountJdeQueue( cb ) {
 
+  var cmd;
 
-// Exclusive use / lock of PDF file established so free to process the file here.
+  cmd = 'umount ' + localJdeDir;
+  exec( cmd, function( err, stdout, stderr ) {
+    if ( err !== null ) {
+      log.debug( cmd + ' - Failed' );
+      cb( null, 'Warning ' + cmd + ' - Failed so either way it is unmounted' );
+    } else {
+      log.debug( cmd + ' - Done' );
+      cb( null, cmd + ' - Done' );
+    }
+  });
+}
 
-function processPDF(record) 
-{
+// Before attempting a mount perform an umount - start from clean state
+function unmountSharedWorkDir( cb ) {
 
-	var jcfndfuf2 = record[0];
-	var jcactdate = record[1];
-	var jcacttime = record[2];
-	var jcprocessid = record[3];
-	var genkey = jcactdate + ' ' + jcacttime;
-	var parms = null;
+  var cmd;
 
-	// Make parameters available to any function in series
-	parms = {'jcfndfuf2': jcfndfuf2, 'record': record, 'genkey': genkey, 'hostname': hostname};
-
-	async.series([
-			function (cb) { createBackupDir(parms, cb) }, 
-			function (cb) { backupPdfFile(parms, cb) }, 	
-			function (cb) { copyPdfToWorkDir(parms, cb) }, 
-			function (cb) { applyLogo(parms, cb) }, 
-			function (cb) { replaceJdePdfWithLogoVersion(parms, cb) },
-			function (cb) { createAuditEntry(parms, cb) },
-			function (cb) { removeLock(parms, cb) }
-			], function (cb) { allDone(parms, cb) }
-	);
-
-
+  cmd = 'umount ' + localWorkDir;
+  exec( cmd, function( err, stdout, stderr ) {
+    if ( err !== null ) {
+      log.debug( cmd + ' - Failed' );
+      cb( null, 'Warning ' + cmd + ' - Failed so either way it is unmounted');
+    } else {
+      log.debug( cmd + ' - Done' );
+      cb( null, cmd + ' - Done' );
+    }
+  });
 }
 
 
+// Check local Jde Directory is actually mounted to the remote Jde Pdf Output Queue
+// It should remain mounted most of the time so issue debug notification if all okay
+// but issue warning notification if mount not in place - auto recovery will activate
+function checkJdeQueueMounted( cb) {
 
+  var cmd;
 
-function createBackupDir(parms, cb) {
+  cmd = 'mountpoint ' + localJdeDir;
+  exec( cmd, function( err, stdout, stderr ) {
+    if ( err !== null ) {
+      log.warn( cmd + ' - Failed' );  
+      cb( err, cmd + ' - Failed' );
+    } else {
+      log.debug( cmd + ' - Done' );
+      cb( null, cmd + ' - Done' );
+    }
+  });
+}
 
-	var cmd = 'mkdir -p /home/shareddata/backup';
+// Check local Jde Directory is actually mounted to the remote Jde Pdf Output Queue
+// It should remain mounted most of the time so issue debug notification if all okay
+// but issue warning notification if mount not in place - auto recovery will activate
+function checkWorkDirMounted( cb) {
 
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Create Backup Directory');
-	console.log(cmd);
-	exec(cmd, function(error, stdout, stderr) {
-		if (error !== null) {
-				cb(error, cmd + ' - Failed');
-			} else {
-				cb(null, cmd + ' - Done');
-			}
-		}
-	);
+  var cmd;
 
+  cmd = 'mountpoint ' + localWorkDir;
+  exec( cmd, function( err, stdout, stderr ) {
+    if ( err !== null ) {
+      log.warn( cmd + ' - Failed' );  
+      cb( err, cmd + ' - Failed' );
+    } else {
+      log.debug( cmd + ' - Done' );  
+      cb( null, cmd + ' - Done' );
+    }
+  });
 }
 
 
-
-// Make a backup copy of the original JDE PDF file - just in case we need the untouched original
-// These can be purged inline with the normal JDE PrintQueue - currentlt PDF's older than approx 2 months
-
-function backupPdfFile(parms, cb) {
-
-	var cmd = 'cp /home/pdfdata/' + parms.jcfndfuf2 + ' /home/shareddata/backup/' + parms.jcfndfuf2;
-
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Copy JDE PDF file to Backup Directory');
-	console.log(cmd);
-	exec(cmd, function(error, stdout, stderr) {
-		if (error !== null) {
-				cb(error, cmd + ' - Failed');
-			} else {
-				cb(null, cmd + ' - Done');
-			}
-		}
-	);
-
-}
-
-
-
-// Make a working copy of the original JDE PDF file - this will have logos added to each page
- 
-function copyPdfToWorkDir(parms, cb) {
-
-	var cmd = 'cp /home/pdfdata/' + parms.jcfndfuf2 + ' /home/shareddata/' + parms.jcfndfuf2.trim() + '_ORIGINAL';
-
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Copy JDE PDF file to work Directory');
-	console.log(cmd);
-	exec(cmd, function(error, stdout, stderr) {
-		if (error !== null) {
-				cb(error, cmd + ' - Failed');
-			} else {
-				cb(null, cmd + ' - Done');
-			}
-		}
-	);
-}
-
-
-
-// Apply logo to working copy of JDE PDF file
-
-function applyLogo(parms, cb) {
-
-	var pdfInput = '/home/shareddata/' + parms.jcfndfuf2.trim() + '_ORIGINAL';
-	var pdfOutput = '/home/shareddata/' + parms.jcfndfuf2;
-	var cmd = 'node ./src/pdfaddlogo.js ' + pdfInput + ' ' + pdfOutput ;
-
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Apply Logo to JDE PDF in Work Directory');
-	console.log(cmd);
-	exec(cmd, function(error, stdout, stderr) {
-		if (error !== null) {
-				cb(error, cmd + ' - Failed');
-			} else {
-				cb(null, cmd + ' - Done');
-			}
-		}
-	);
-}
-
-
-
-// Replace original JDE PDF File in PrintQueue with amended PDF incuding logos
-
-function replaceJdePdfWithLogoVersion(parms, cb) {
-
-	var pdfWithLogos = '/home/shareddata/' + parms.jcfndfuf2;
-	var jdePrintQueue = '/home/pdfdata/' + parms.jcfndfuf2;
-	var cmd = 'mv ' + pdfWithLogos + ' ' + jdePrintQueue;
-
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Replace JDE PDF with modified Logo version');
-	console.log(cmd);
- 	exec(cmd, function(error, stdout, stderr) {
-		if (error !== null) {
-				cb(error, cmd + ' - Failed');
-			} else {
-				cb(null, cmd + ' - Done');
-			}
-		}
-	);
-}
-
-
-
-
-function createAuditEntry(parms, cb) {
-
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Write Audit Record');
-
-	// Cretae Audit entry for this Processed record
-	audit.createAuditEntry(parms.jcfndfuf2, parms.genkey, parms.hostname, 'PROCESSED - LOGO');
-
-	cb(null, 'Lock Released');
-
-}
-
-
-function removeLock(parms, cb) {
-
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Release Lock');
-	lock.removeLock(parms.record, parms.hostname);
-	console.log('Processing PDF ' + parms.jcfndfuf2 + ' - Lock Released');
-
-	cb(null, 'Lock Released');
-
-}
-
-
-function allDone(err, results) {
-
-	console.log('ALL DONE');
-
-}
 
 
